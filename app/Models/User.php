9febb1fafde939ad1bab24ccd4078dd74b80de17
Ann <annie.ga@yandex.ru>
2022-04-12 17:50:55 +0300

    different roles

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function role() {
+        return $this->belongsTo(Role::class);
+    }
+
+    public function getIsManagerAttribute() {
+        return $this->role->code === Role::MANAGER;
+    }
 }
